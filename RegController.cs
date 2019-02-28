19b326d87738f5d5336e47a2156aca72c51df845
reeba-alex <47102673+reeba-alex@users.noreply.github.com>
2019-02-28 16:31:42 +0530

    Add files via upload

---
+++ RegController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace reg.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getstudent()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace reg.Models
+{
+    public class Student
+    {
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student name")]
+        // [RegularExpression()]
+
+        public string Sname { get; set; }
+
+        [Required]
+        [Display(Name = "Student address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+
+        public string Gender { get; set; }
+        public Course SCourse { get; set; }
+        [Display(Name = "Upload Image")]
+        public string Image { get; set; }
+
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        [Required]
+        [Display(Name = "Student email")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student username")]
+        public string Uname { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "confirm password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+    public enum Course
+    {
+        MCA,
+        MBA,
+        MTECH
+    }
+}
+
+    
